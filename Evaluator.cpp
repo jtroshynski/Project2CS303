1f59ca71090318bbec736b6f5378f7ff08774a69
jtroshynski <jtroshynski@gmail.com>
2016-07-11 13:23:56 -0500

    Adding basic function definitions

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -10,3 +10,21 @@ using namespace std;
 
 Evaluator::Evaluator(){};
 
+bool Evaluator::check_bool(string expression)
+{
+	//if expression contains boolean characters, return true
+}
+
+bool Evaluator::eval_bool(string expression)
+{
+	//call function to parse string into two stacks, one for integers and another for characters
+
+  //
+}
+
+int Evaluator::eval_int(string expression)
+{
+	//call function to parse string into two stacks, one for integers and another for characters
+
+	//
+}
