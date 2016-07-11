17591ca61f377b0ea31ebc6c23611b4af56c88bf
jtroshynski <jtroshynski@gmail.com>
2016-07-11 13:24:47 -0500

    Updating main() handling of bool/integer values

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@
 //Project 2A
 
 #include "Evaluator.h"
+#include <string>
 using namespace std;
 
 int main(){
-	Evaluator eval;
-	int result = eval.eval("1+2+3-4");
+	Evaluator eval1;
+	string expression = ("1+2+3+4"); //enter in any expression in string format
+	eval1.check_bool(expression); //check if expression should be evaluated as a boolean or an integer value. True = boolean, False = integer
+
+	if (eval1.check_bool(expression)) //boolean
+	{
+		bool bool_result = eval1.eval_bool(expression);
+	}
+	else //integer
+	{
+		int int_result = eval1.eval_int(expression);
+	}
 
 	return 0;
 }
