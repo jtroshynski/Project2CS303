42ee63c2ee3682c0349ebfe37f7c3b5f1351f2fa
yamesmon <yamesmon@gmail.com>
2016-07-14 01:45:22 -0500

    added some private prototypes and one new public function
    I realize you already wrote a similar function (check_bool()) Jeremy but I thought we should consider using an array of characters (which is a string) instead of a string type. I think the precedence will be easier to handle. Please delete if you disagree--just a thought.

--- Evaluator.h
+++ Evaluator.h
@@ -8,9 +8,11 @@
 #define _EVALUATOR_H_
 
 #include <string>
+#include <stack>
 #include "list.h"
 #include "list_iterator.h"
 #include "list_const_iterator.h"
+
 using namespace std;
 
 class Evaluator
@@ -18,12 +20,43 @@ class Evaluator
 private:
 	list <int> int_stack;
 	list <char> char_stack;
+	
+	// Data fields
+		static const string OPERATORS;
+		std::stack<int> operand_stack;
+		static const int PRECEDENCE[];
+
+
+	/** Evaluates the current operator.
+	This function pops the two operands off the operand
+	stack and applies the operator.
+	@param op A character representing the operator
+	@throws Syntax_Error if top is attempted on an empty stack
+	*/
+	int eval_op(char op);
+	/** Determines whether a character is an operator.
+	@param ch The character to be tested
+	@return true if the character is an operator
+	*/
+	bool is_operator(char ch) const {
+		return OPERATORS.find(ch) != string npos;	
+	}
+	/** Determines the precedence of an operator.
+	@param op The operator
+	@return The precedence
+	*/
+	int precedence(char op) const {
+		return PRECEDENCE[OPERATORS.find(op)];
+	}
+
 
 public:
 	Evaluator();
+	
 	bool check_bool(string expression);
 	bool eval_bool(string expression);
 	int eval_int(string expression);
+	int parse_expression(string expression);
 	void parse_expression(string expression);
 	//Michelle Got this from GitHub by kartikkukreja
 	bool hasLowerPriority(char op1, char op2);
