ca611353db2b074f1fedb3815b35d80ca18358b1
yamesmon <yamesmon@gmail.com>
2016-07-17 04:04:30 -0500

    Updating for testing (making it run-able)
    Added some comments for deletes that probably could be made, but I will let one of you actually delete them

--- Evaluator.h
+++ Evaluator.h
@@ -9,17 +9,20 @@
 
 #include <string>
 #include <stack>
-#include "list.h"
-#include "list_iterator.h"
-#include "list_const_iterator.h"
+
+//if we don't need these list files and you guys concur go ahead and delete
+//#include "list.h"
+//#include "list_iterator.h"
+//#include "list_const_iterator.h"
 
 using namespace std;
 
 class Evaluator
 {
 private:
-	list <int> int_stack;
-	list <char> char_stack;
+	////if we don't need this list stuff and you guys concur go ahead and delete
+	//list <int> int_stack;
+	//list <char> char_stack;
 	
 	// Data fields
 		static const string OPERATORS;
@@ -38,16 +41,15 @@ private:
 	@param op A character representing the operator
 	@throws Syntax_Error if top is attempted on an empty stack
 	*/
-	int eval_op(char op);{
+	int eval_op(char next_char);
 		
-	}
 	
 	/** Determines whether a character is an operator.
 	@param ch The character to be tested
 	@return true if the character is an operator
 	*/
 	bool is_operator(char ch) const {
-		return OPERATORS.find(ch) != string npos;	
+		return OPERATORS.find(ch) != string::npos;	
 	}
 	
 	/** Determines the precedence of an operator.
@@ -60,43 +62,49 @@ private:
 	
 	/** Determines if expression has unmatching parenthesis.
 	@param op The expression
-	@return The precedence
+	@return bool
 	*/
-	bool is_balanced(const string expression) {
-		return PRECEDENCE[OPERATORS.find(op)];
-	}
+	bool is_balanced(const string expression);
+	//	return PRECEDENCE[OPERATORS.find(op)];
 	
-	/** Determines whether characters in string are operands or aoperators
-	  and adds them to their respective appropriate stacks--
+	
+	/** Determines whether characters in string are operands or operators
+	  and adds them to their respective stacks--
 	@param op The expression
 	@return two separate stacks operator_stack and operand_stack.
 	*/
-	int parse_expression(string expression); {
-	
-	}
-	bool check_valid(string str); {
-	
-	}
+	int parse_expression(string expression); 
 	
 	/** Determines '--' or '++' occur in the expression and adds one
 	 * to the successor if so.
 	@param op The expression
 	@return updated integer.
 	*/
-	int decrementIncrement(string str); {
-	
-	}
+	int decrementIncrement(string str); 
 
 	
 	
 public:
 	Evaluator();
 	
+	/** Exception handling:
+	//Expression must have correct beginning parenthesis
+	//Expression cannot contain letters
+	//Expression cannot start with a binary operator
+	//Expression cannot contain two binary operators in a row
+	//Expression cannot contain two operands in a row (15 + 2 3)
+	//A unary operand cannot be followed by a binary operator (++>3)
+	//Simple division by zero
+	@param op The expression
+	@return error*/
+	bool check_valid(string expression);
+	
 	bool check_bool(string expression);
 	bool eval_bool(string expression);
 	int eval_int(string expression);
 
-	void parse_expression(string expression);
+	//void parse_expression(string expression);
+	
 	//Michelle Got this from GitHub by kartikkukreja
 	bool hasLowerPriority(char op1, char op2);
 	int convert_to_int(string number);
